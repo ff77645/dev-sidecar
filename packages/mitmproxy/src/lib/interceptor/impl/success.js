89f8122f539c452363a08ed8bc366b0eac39443f
fy <fy77645@126.com>
2023-07-25 18:28:46 +0800

    强化请求拦截功能

--- packages/mitmproxy/src/lib/interceptor/impl/success.js
+++ packages/mitmproxy/src/lib/interceptor/impl/success.js
@@ -2,9 +2,16 @@ module.exports = {
   requestIntercept (context, interceptOpts, req, res, ssl, next) {
     const { rOptions, log } = context
     log.info('success:', rOptions.hostname, req.url)
-    res.writeHead(200)
-    res.write('DevSidecar 200: \n\n request success, this request is matched by success intercept.\n\n 因配置success拦截器，本请求将直接返回成功')
-    res.end()
+    if (interceptOpts.json) {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      })
+      res.end(JSON.stringify(interceptOpts.json))
+    } else {
+      res.writeHead(200)
+      res.write('DevSidecar 200: \n\n request success, this request is matched by success intercept.\n\n 因配置success拦截器，本请求将直接返回成功')
+      res.end()
+    }
     return true// 是否结束
   },
   is (interceptOpt) {
